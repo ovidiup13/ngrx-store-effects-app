37ee22b1b14044eb84d232004e4adea12f5129ee
Ovidiu Popoviciu <devovip13@gmail.com>
2017-12-28 13:48:08 +0200

    Move selectors to own folder. Add feature and route state selector

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,8 +1,4 @@
-import {
-  ActionReducerMap,
-  createSelector,
-  createFeatureSelector
-} from "@ngrx/store";
+import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
 
 import * as fromPizzas from "./pizzas.reducers";
 import { getPizzasEntities } from "./pizzas.reducers";
@@ -17,29 +13,4 @@ export const reducers: ActionReducerMap<ProductsState> = {
 
 export const getProductsState = createFeatureSelector<ProductsState>(
   "products"
-);
-
-// pizzas state
-export const getPizzasState = createSelector(
-  getProductsState,
-  (state: ProductsState) => state.pizzas
-);
-
-// only pass the slices that we need
-export const getAllPizzasEntities = createSelector(
-  getPizzasState,
-  fromPizzas.getPizzasEntities
-);
-
-export const getAllPizzas = createSelector(getAllPizzasEntities, entities => {
-  return Object.keys(entities).map(id => entities[parseInt(id)]);
-});
-
-export const getAllPizzasLoaded = createSelector(
-  getPizzasState,
-  fromPizzas.getPizzasLoaded
-);
-export const getAllPizzasLoading = createSelector(
-  getPizzasState,
-  fromPizzas.getPizzasLoading
 );
---
+++ src/products/store/selectors/pizzas.selectors.ts
@@ -0,0 +1,40 @@
+import { createSelector } from "@ngrx/store";
+
+import * as fromRoot from "../../../app/store";
+import * as fromFeature from "../reducers/";
+import * as fromPizzas from "../reducers/pizzas.reducers";
+import { getPizzasEntities } from "../reducers/pizzas.reducers";
+import { Pizza } from "src/products/models/pizza.model";
+
+export const getPizzasState = createSelector(
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductsState) => state.pizzas
+);
+
+// only pass the slices that we need
+export const getAllPizzasEntities = createSelector(
+  getPizzasState,
+  fromPizzas.getPizzasEntities
+);
+
+// use the router state to look up an entity
+export const getSelectedPizza = createSelector(
+  getAllPizzasEntities, // feature state
+  fromRoot.getRouterState, // router state
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+export const getAllPizzas = createSelector(getAllPizzasEntities, entities => {
+  return Object.keys(entities).map(id => entities[parseInt(id)]);
+});
+
+export const getAllPizzasLoaded = createSelector(
+  getPizzasState,
+  fromPizzas.getPizzasLoaded
+);
+export const getAllPizzasLoading = createSelector(
+  getPizzasState,
+  fromPizzas.getPizzasLoading
+);
