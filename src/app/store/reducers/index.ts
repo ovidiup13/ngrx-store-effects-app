e6a4a7a6f9beaf9a86b52a2aaec165024702a867
Ovidiu Popoviciu <devovip13@gmail.com>
2017-12-28 13:18:32 +0200

    Add router state serializer and link it to app module

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -2,8 +2,13 @@ import * as fromRouter from "@ngrx/router-store";
 
 import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
 
-import { Params } from "@angular/router";
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params
+} from "@angular/router";
 
+// can be extended => make sure to update the serializer
 export interface RouterStateUrl {
   url: string;
   queryParams: Params;
@@ -22,3 +27,24 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState<RouterStateUrl>
 >("routerReducer");
+
+// serializer called everytime route changes
+// get a new state, wherever you are, at all times
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    // de-structure import
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    // traverse the state tree of angular router
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+
+    const { params } = state;
+
+    return { url, queryParams, params };
+  }
+}
