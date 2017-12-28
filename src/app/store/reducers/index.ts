8af781f8ab185973e50cb9a0043146c15a762c4a
Ovidiu Popoviciu <devovip13@gmail.com>
2017-12-28 12:49:13 +0200

    Add router store

---
+++ src/app/store/reducers/index.ts
@@ -0,0 +1,24 @@
+import * as fromRouter from "@ngrx/router-store";
+
+import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
+
+import { Params } from "@angular/router";
+
+export interface RouterStateUrl {
+  url: string;
+  queryParams: Params;
+  params: Params;
+}
+
+export interface State {
+  routerReducer: fromRouter.RouterReducerState<RouterStateUrl>;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  routerReducer: fromRouter.routerReducer
+};
+
+// selector for router state
+export const getRouterState = createFeatureSelector<
+  fromRouter.RouterReducerState<RouterStateUrl>
+>("routerReducer");
