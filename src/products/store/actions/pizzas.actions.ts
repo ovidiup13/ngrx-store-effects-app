a1ff230e95bb4f0fdd4c5a39e605d6e6336790f2
Ovidiu Popoviciu <devovip13@gmail.com>
2018-01-08 15:58:16 +0000

    Add actions for update pizza

--- src/products/store/actions/pizzas.actions.ts
+++ src/products/store/actions/pizzas.actions.ts
@@ -23,6 +23,7 @@ export class LoadPizzasSuccess implements Action {
 
 //action types
 
+// create pizza
 export const CREATE_PIZZA = "[Products] Create Pizza";
 export const CREATE_PIZZA_FAIL = "[Products] Create Pizza Fail";
 export const CREATE_PIZZA_SUCCESS = "[Products] Create Pizza Success";
@@ -42,10 +43,34 @@ export class CreatePizzaSuccess implements Action {
   constructor(public payload: Pizza) {}
 }
 
+// update pizza
+
+export const UPDATE_PIZZA = "[Products] Update Pizza";
+export const UPDATE_PIZZA_FAIL = "[Products] Update Pizza Fail";
+export const UPDATE_PIZZA_SUCCESS = "[Products] Update Pizza Success";
+
+export class UpdatePizza implements Action {
+  readonly type: string = UPDATE_PIZZA;
+  constructor(public payload: Pizza) {}
+}
+
+export class UpdatePizzaFail implements Action {
+  readonly type: string = UPDATE_PIZZA;
+  constructor(public payload: any) {}
+}
+
+export class UpdatePizzaSuccess implements Action {
+  readonly type: string = UPDATE_PIZZA;
+  constructor(public payload: Pizza) {}
+}
+
 export type PizzasActions =
   | LoadPizzas
   | LoadPizzasFail
   | LoadPizzasSuccess
   | CreatePizza
   | CreatePizzaFail
-  | CreatePizzaSuccess;
+  | CreatePizzaSuccess
+  | UpdatePizza
+  | UpdatePizzaFail
+  | UpdatePizzaSuccess;
